dc80547f436cdb4bc5144a325095c2c3d56b58e1
Sebastian Llamuca <llamucasebas@gmail.com>
2024-03-09 19:11:36 -0500

    feat: create text styled component

--- src/styled-component/Text.jsx
+++ src/styled-component/Text.jsx
@@ -8,3 +8,27 @@ export const Title = styled.h2`
 export const Link = styled(_Link)`
   text-decoration: underline;
 `
+
+export const Text = styled.p.attrs(props => ({
+  $fontSize: props.$fontSize || '1em',
+  $color: props.$color || 'black',
+  $fontFamily: props.$fontFamily || 'inherit',
+  $weight: props.$weight || 'normal',
+  $lineHeight: props.$lineHeight || '1.5',
+  $textDecoration: props.$textDecoration || 'none',
+  $align: props.$align || 'left',
+  $letterSpacing: props.$letterSpacing || 'normal',
+  $wordSpacing: props.$wordSpacing || 'normal',
+  $textTransform: props.$textTransform || 'none'
+}))`
+  font-size: ${props => props.$fontSize};
+  color: ${props => props.$color};
+  font-family: ${props => props.$fontFamily};
+  font-weight: ${props => props.$weight};
+  line-height: ${props => props.$lineHeight};
+  text-decoration: ${props => props.$textDecoration};
+  text-align: ${props => props.$align};
+  letter-spacing: ${props => props.$letterSpacing};
+  word-spacing: ${props => props.$wordSpacing};
+  text-transform: ${props => props.$textTransform};
+`
